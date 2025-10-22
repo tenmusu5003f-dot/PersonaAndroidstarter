da7115137c53a6415b403364a481f968cdb2a792
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 22:21:43 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
-    // ここは「ルート用」。apply false でOK（実体は app 側で有効化）
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.js") version "1.9.23"
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            binaries.executable()
+        }
+    }
+    sourceSets {
+        val main by getting {
+            dependencies {
+                implementation(project(":persona_core"))
+            }
+        }
+    }
 }
