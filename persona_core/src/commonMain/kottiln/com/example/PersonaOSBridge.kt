7652e62f600fb25de90a30a3a9011f938d6dffbf
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-23 02:30:11 +0900

    Delete persona_core/src/commonMain/kottiln/com/example directory

--- persona_core/src/commonMain/kottiln/com/example/PersonaOSBridge.kt
+++
@@ -1,9 +0,0 @@
-package com.example.persona.core
-interface PersonaOSBridge {
-    fun log(message: String)
-    fun nowMillis(): Long
-}
-class DefaultOSBridge : PersonaOSBridge {
-    override fun log(message: String) { /* no-op */ }
-    override fun nowMillis(): Long = 0L
-}
