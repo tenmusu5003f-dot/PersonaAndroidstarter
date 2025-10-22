a069808c6aae87c31ac84a4de6b31fb9e4335678
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-23 00:01:47 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,8 +31,3 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
-<SwitchPreferenceCompat
-    android:key="auto_backup_enabled"
-    android:title="自動バックアップを有効にする"
-    android:summary="オンにすると毎日自動でバックアップを行います" />
