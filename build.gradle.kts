18bb11f88a7a424415b64223d0242e15a1213a05
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 22:27:06 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,47 @@ allprojects {
         kotlinOptions.jvmTarget = "17"
     }
 }
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "1.9.23"
+    id("com.android.library")
+}
+
+kotlin {
+    androidTarget()
+
+    // iOS（フレームワーク出力／将来Swiftから利用）
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    jvm("desktop") // Desktop用
+    js(IR) { browser() }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting
+
+        val androidMain by getting
+        val androidUnitTest by getting
+
+        val iosMain by getting
+        val iosTest by getting
+
+        val desktopMain by getting
+        val desktopTest by getting
+
+        val jsMain by getting
+        val jsTest by getting
+    }
+}
+
+android {
+    namespace = "com.example.persona.core"
+    compileSdk = 34
+    defaultConfig { minSdk = 24 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
