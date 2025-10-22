9475788d551d6ad236857b44ccdef955ead8de38
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 22:56:59 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,13 @@ plugins {
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
+
+// Project-level build.gradle.kts
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
