15a1e96d8edb9041109fe1b13a5542f8430a23fa
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 22:40:22 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,4 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "PersonaAndroidstarter"
-include(":app")
+rootProject.name = "PersonaAndroid"
 
 pluginManagement {
     repositories {
@@ -29,34 +14,3 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "PersonaAndroidstarter"
-include(":app")
-
-// ★ ここでローカル/CIのビルドキャッシュをON
-buildCache {
-    local {
-        isEnabled = true
-    }
-}
-
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "PersonaSystem"
-
-include(":persona_core")
-include(":androidApp")
-include(":desktopApp")
-include(":webApp")
-// （iOSはXcodeで使うためGradle側にモジュール追加不要。persona_core の iOSターゲットだけでOK）
