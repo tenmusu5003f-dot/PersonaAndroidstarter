3d19977845892b061cfbd3eacc510c9324eb89f2
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 21:05:00 +0900

    settings.gradle.kts の更新

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,27 @@ dependencyResolutionManagement {
 }
 rootProject.name = "PersonaAndroidstarter"
 include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "PersonaAndroidstarter"
+include(":app")
+
+// ★ ここでローカル/CIのビルドキャッシュをON
+buildCache {
+    local {
+        isEnabled = true
+    }
+}
