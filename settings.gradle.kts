8077844ed2617fb0f11190a1de8453bc807c2ac4
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-22 22:25:23 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,3 +38,25 @@ buildCache {
         isEnabled = true
     }
 }
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "PersonaSystem"
+
+include(":persona_core")
+include(":androidApp")
+include(":desktopApp")
+include(":webApp")
+// （iOSはXcodeで使うためGradle側にモジュール追加不要。persona_core の iOSターゲットだけでOK）
