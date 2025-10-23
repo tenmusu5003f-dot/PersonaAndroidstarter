ae6dfa3011bb0658321a97a3a531dec02ac45860
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 01:22:21 +0900

    Create settings.gradle.kts

---
+++ _roads/kt/app/settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PersonaAndroidstarter"
+
+// ← ここ重要！ app の実パスを明示
+include(":app")
+project(":app").projectDir = file("_roads/kt/app")
