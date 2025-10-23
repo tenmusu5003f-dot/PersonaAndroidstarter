f65d0b35f70ad18e187ce9cc7f8354ee56583438
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 04:47:50 +0900

    Update build.gradle.kts

--- kt/build.gradle.kts
+++ kt/build.gradle.kts
@@ -1,39 +1,20 @@
-plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
-}
+include(":app")
+project(":app").projectDir = file("kt/app")
 
-android {
-  namespace = "com.persona.androidstarter"
-  compileSdk = 34
-
-  defaultConfig {
-    applicationId = "com.persona.androidstarter"
-    minSdk = 24
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions { jvmTarget = "17" }
 }
 
-dependencies {
-  implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.appcompat:appcompat:1.7.0")
-  implementation("com.google.android.material:material:1.12.0")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
+rootProject.name = "PersonaAndroidstarter"
