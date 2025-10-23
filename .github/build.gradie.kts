ffa566f401976d9448a984616e7308b85ede2e00
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-23 23:32:52 +0900

    Update build.gradie.kts

--- .github/build.gradie.kts
+++ .github/build.gradie.kts
@@ -1,8 +1,17 @@
 plugins {
-  id("com.android.application") version "8.1.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
 }
 
-tasks.register("clean", Delete::class) {
-  delete(rootProject.buildDir)
+android {
+    namespace "com.example.persona"
+    compileSdk 34
+
+    defaultConfig {
+        applicationId "com.example.persona"
+        minSdk 24
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
+    }
 }
