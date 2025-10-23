83aadcad0af906f274dcf677ee54510fb9a92278
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 02:08:53 +0900

    Create settings.gradle.kts

---
+++ .github/workflows/settings.gradle.kts
@@ -0,0 +1,18 @@
+include(":app")
+project(":app").projectDir = file("_roads/kt/app")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "PersonaAndroidstarter"
