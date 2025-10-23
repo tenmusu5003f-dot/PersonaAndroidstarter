cb3814b362c84d6eb5efcc809b2b0eaf8176a157
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 07:05:15 +0900

    Refactor build.gradle.kts to update dependencies

--- kt/app/build.gradle.kts
+++ kt/app/build.gradle.kts
@@ -24,40 +24,6 @@ android {
             )
         }
     }
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-}
-
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.persona.androidstarter"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.persona.androidstarter"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
