041056f4e63a14f3d1d115e9010bed337a88893e
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-23 17:29:32 +0900

    Update settings.gradle.kts

--- .github/workflows/settings.gradle.kts
+++ .github/workflows/settings.gradle.kts
@@ -1,3 +1,19 @@
+gradle init \
+
+--type java-application \
+
+--dsl kotlin \
+
+--test-framework junit-jupiter \
+
+--package my.project \
+
+--project-name my project \
+
+--no-split-project \
+
+--java-version 17
+
 rootProject.name = "PersonaAndroidStarter"
 
 // モジュール名は一般的な :app に固定して、物理フォルダを「アプリ」にマッピング
