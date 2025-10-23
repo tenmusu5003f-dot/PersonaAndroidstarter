654e7f0447918f5af96b41fc0788eca5dc64204a
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-23 17:33:44 +0900

    Update build.gradle.kts

--- .github/workflows/build.gradle.kts
+++ .github/workflows/build.gradle.kts
@@ -14,6 +14,26 @@ gradle init \
 
 --java-version 17
 
+> Task :tasks
+
+------------------------------------------------------------
+Tasks runnable from root project 'myTutorial'
+------------------------------------------------------------
+
+Build Setup tasks
+-----------------
+init - Initializes a new Gradle build.
+wrapper - Generates Gradle wrapper files.
+
+Help tasks
+----------
+buildEnvironment - Displays all buildscript dependencies declared in root project 'myTutorial'.
+...
+
+33920 JPS
+27171 グラドルデーモン
+22792
+
 plugins {
     // ここは空でもOK（ルートでは基本的に依存解決だけ）
 }
