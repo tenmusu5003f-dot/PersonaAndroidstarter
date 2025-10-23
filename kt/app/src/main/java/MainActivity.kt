ee04618241cf2b0bc7e90377bc9ce239b27871bb
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 05:24:26 +0900

    Update MainActivity.kt

--- kt/app/src/main/java/MainActivity.kt
+++ kt/app/src/main/java/MainActivity.kt
@@ -1,1 +1,16 @@
 
+package com.persona.androidstarter
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import core.ui.HomeMenu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 起動時に HomeMenu を呼び出す
+        startActivity(Intent(this, HomeMenu::class.java))
+        finish()
+    }
+}
