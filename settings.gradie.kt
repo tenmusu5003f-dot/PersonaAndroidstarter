aa2c0046c8d2086e1f27447ccfdc546a8861bdff
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 17:42:14 +0900

    Update settings.gradie.kt

--- settings.gradie.kt
+++ settings.gradie.kt
@@ -1,17 +1,18 @@
 pluginManagement {
     repositories {
-        google()       // ← これがないと Android Plugin が見つからない！
+        google()            // ← これが無いとAGPが見つからない
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-pluginManagement {
-  repositories { gradlePluginPortal(); google(); mavenCentral() }
-}
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories { google(); mavenCentral() }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "PersonaAndroidstarter"
 include(":app")
