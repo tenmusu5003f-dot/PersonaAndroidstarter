a9ae17e3c450b1279d88b3f85300d763cae9b297
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 10:05:53 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+android {
+    sourceSets["main"].java.srcDirs(
+        "kt/app/main/java",
+        "kt/app/src/main/java/kotlin"
+    )
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
