afaa7b7c35751faa5a3b88473cd68fac326d7073
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 07:46:59 +0900

    Update build.gradle.kts

--- kt/app/build.gradle.kts
+++ kt/app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.5.2"
+    kotlin("android") version "1.9.24" apply false
 }
 
 android {
-    namespace = "com.persona.androidstarter"
+    namespace = "com.example.persona"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.persona.androidstarter"
+        applicationId = "com.example.persona"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -24,17 +24,14 @@ android {
             )
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    buildFeatures {
+        viewBinding = true
     }
-    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
