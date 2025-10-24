81981dcd1433ab1eec1d4085a932590a780ff2ba
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 14:43:45 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // ここでは「apply false」で定義だけ。各モジュール側でapplyする
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+  id("com.android.application") version "8.1.0" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
