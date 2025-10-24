9cf7e633fd48f8ff14ee0dda861cac8471ad0d80
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 08:47:28 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+android {
+    compileSdkVersion 34  // ← 最新を推奨
+    defaultConfig {
+        applicationId "com.example.persona"
+        minSdkVersion 26    // ★ここを26以上に
+        targetSdkVersion 34
+        ...
+    }
+}
+
 plugins {
     id("com.android.application") version "8.5.2" apply false
     kotlin("android") version "1.9.24" apply false
