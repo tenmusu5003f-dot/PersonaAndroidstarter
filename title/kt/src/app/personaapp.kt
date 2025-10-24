563bea8a828f00ce0da4ad974f07e6f43ceff098
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-24 18:36:28 +0900

    Initialize PersonaCore in PersonaApp on creation

--- title/kt/src/app/personaapp.kt
+++ title/kt/src/app/personaapp.kt
@@ -1,1 +1,12 @@
+package com.persona.androidstarter
+import android.app.Application
+import android.util.Log
+
+class PersonaApp : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    Log.i("PersonaApp","Persona system boot")
+    PersonaCore.initialize(this) // ← 既存コア初期化
+  }
+}
 
