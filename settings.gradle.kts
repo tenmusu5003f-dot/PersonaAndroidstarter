53ba6439470154862dabd796a3658f460cfd8607
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 19:26:41 +0900

    Correct dependencyResolutionManagement syntax
    Fix syntax error in dependencyResolutionManagement block.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 // build.gradle.kts (project root)
 
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement {
+dependencyResolutionManagement { "(Line: 1, Col: 1): A sequence was not expected" ｝
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
 }
