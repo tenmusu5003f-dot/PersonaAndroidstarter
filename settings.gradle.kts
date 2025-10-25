d46200f9fb9a79f2af59f701c3cc9c187dd64ceb
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 22:39:07 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 // build.gradle.kts (project root)
 
+plugins {
+  id("com.android.application") version "8.5.2" apply false
+  // もし kotlin plugin も同様に書かれているなら、そちらも合わせる
+}
+
 rootProject.name = "PersonaAndroidstarter"
 include(":app")
 project(":app").projectDir = file("kt/app")
