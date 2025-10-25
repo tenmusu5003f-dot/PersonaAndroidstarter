027e841b3b415b1dcc5c61d0de75718552a81901
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 10:22:36 +0900

    Update build.gradle.kts

--- kt/app/build.gradle.kts
+++ kt/app/build.gradle.kts
@@ -1,6 +1,14 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+android {
+    namespace = "com.example.persona"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.persona"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
 
 dependencies {
