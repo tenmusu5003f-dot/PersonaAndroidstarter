abb21ee93f78c9b9909d70b954df84f7ac70e4d4
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 19:57:21 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 // build.gradle.kts (project root)
 
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { "(Line: 1, Col: 1): A sequence was not expected" ｝
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
-}
+rootProject.name = "PersonaAndroidstarter"
+include(":app")
+project(":app").projectDir = file("kt/app")
