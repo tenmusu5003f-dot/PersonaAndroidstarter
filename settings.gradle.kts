c136ef4e1c47b50e2abf5d306334aa5ecd523f90
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 17:40:54 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,7 @@
 
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("ord gradle") version "1.9.22" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false　
 }
