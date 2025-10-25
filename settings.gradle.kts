6d3faae98b0a31ca3ef8b4b7db9053f74c0e74a3
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 10:12:06 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,14 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
     plugins {
         id("com.android.application") version "8.5.2"
         id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
 }
-
 rootProject.name = "PersonaAndroidstarter"
 include(":app")
-project(":app").projectDir = file("kt/app") // ← モジュールは kt/app に固定
+project(":app").projectDir = file("kt/app")
