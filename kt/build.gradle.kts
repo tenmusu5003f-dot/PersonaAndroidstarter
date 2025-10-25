0508e431d99582bf644e8159c0c8ad2c44fb6bf7
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 10:04:47 +0900

    Update build.gradle.kts

--- kt/build.gradle.kts
+++ kt/build.gradle.kts
@@ -1,2 +1,8 @@
 // root build.gradle.kts
 plugins { }
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+}
