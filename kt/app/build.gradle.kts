4ea17bda543f42f18a447fea27a2ee00bdbbdaa7
tenmusu5003f-dot <tenmusu5003f@gmail.com>
2025-10-25 10:17:02 +0900

    Update build.gradle.kts

--- kt/app/build.gradle.kts
+++ kt/app/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
+
 android {
     namespace = "com.persona.androidstarter"
     compileSdk = 34
